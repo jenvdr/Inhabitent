2444db077903d082d6238e8a5309fb40972f2f40
Jen <vobis.jennifer@yahoo.co.uk>
2019-05-30 23:13:55 +0100

    journal page laid out

--- themes/inhabitent/home.php
+++ themes/inhabitent/home.php
@@ -1,33 +1,51 @@
 
 <?php
 /**
- * The template for displaying all pages
+ * The main template file
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package inhabitent
+ * @package Inhabitents
  */
-// JOURNAL PAGE
 get_header();
 ?>
-<div class = "isthisthefrontPAGE">
-<?php
-while ( have_posts() ) :
-				the_post();
-				the_title();
-				the_excerpt();
-				/*
+<div id="sidebar-layout">
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main">
+
+      <?php
+      if (have_posts()) :
+        if (is_home() && !is_front_page()) :
+          ?>
+          <header>
+            <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+          </header>
+        <?php
+      endif;
+      /* Start the Loop */
+	  while (have_posts()) :
+		the_post();
+        /*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content-journal', get_post_type() );
-			endwhile;
-get_footer();
-?>
-</div>
+        get_template_part('template-parts/content', "journal");
+      endwhile;
+      the_posts_navigation();
+    else :
+      get_template_part('template-parts/content', 'none');
+    endif;
+    ?>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+  <?php
+  get_sidebar();
+  get_footer();
+  ?>
