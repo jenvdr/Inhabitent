10c75e38421d1afaa7447a74d0fe74962d941269
Jen <vobis.jennifer@yahoo.co.uk>
2019-06-02 23:32:12 +0100

    finished home page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -23,6 +23,7 @@ get_header();
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg" alt="Inhabitent Logo">
       </section>
 
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -30,8 +31,39 @@ get_header();
 			get_template_part( 'template-parts/content', 'page' );
         ?>
 
-         <h2>Inhabitent Journal</h2>
-         <div class="blogposts">
+              <!-- SHOP STUFF --->
+              <section class="shop-stuff">
+          <h2>Shop Stuff</h2>
+          <div class="product-blocks">
+            <div class="product-block-wrapper">
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/product-type-icons/do.svg" alt="Do">
+              <p>Get back to nature with all the tools and toys you need to enjoy the great outdoors.</p>
+              <p><a href="http://localhost:8887/inhabitent-project/producttype/do/" class="btn">Do Stuff</a></p>
+            </div>
+          <div class="product-block-wrapper">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/product-type-icons/eat.svg" alt="Eat">
+            <p>Nothing beats food cooked over a fire. We have all you need for good camping eats.</p>
+            <p><a href="http://localhost:8887/inhabitent-project/producttype/eat/" class="btn">Eat Stuff</a></p>
+          </div>
+          <div class="product-block-wrapper">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/product-type-icons/sleep.svg" alt="Sleep">
+            <p>Get a good night's rest in the wild in a home away from home that travels well.</p>
+            <p><a href="http://localhost:8887/inhabitent-project/producttype/sleep/" class="btn">Sleep Stuff</a></p>
+          </div>
+            <div class="product-block-wrapper">
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/product-type-icons/wear.svg" alt="Wear">
+              <p>From flannel shirts to toques, look the part while roughing it in the great outdoors.</p>
+              <p><a href="http://localhost:8887/inhabitent-project/producttype/wear/" class="btn">Wear Stuff</a></p>
+            </div>
+          </div>
+        </section>
+
+
+        <!-- JOURNAL STUFF -->
+        <div class="journal-stuff">
+        <h2>Inhabitent Journal</h2>
+    </div>
+        <div class="blogposts">
         <?php
 $args = array( 'post_type' => 'post', 'order' => 'ASC', 'posts_per_page' => 3);
    $products = new WP_Query( $args ); // instantiate our object
@@ -64,6 +96,7 @@ $args = array( 'post_type' => 'post', 'order' => 'ASC', 'posts_per_page' => 3);
       <!-- instantiate ADVENTURES --->
 
       <div class="adventures">
+      <h2>Latest Adventures</h2>
       <?php
          $args = array( 'post_type' => 'adventure', 'order' => 'ASC', 'posts_per_page' => 4); //<——— ADD AMOUNT
 
@@ -75,10 +108,10 @@ $args = array( 'post_type' => 'post', 'order' => 'ASC', 'posts_per_page' => 3);
         <?php while ( $adventures->have_posts() ) : $adventures->the_post(); ?>
         <li>
           <div class="adventure-wrapper">
-            <img href=<?php echo the_post_thumbnail( 'large' ) ?>
+            <img href=<?php echo the_post_thumbnail( 'full' ) ?>
           <div class="post-info-wrapper">
             <h3><?php echo the_title() ?></h3>
-          <button class="read-entry"><a href="<?php echo the_permalink() ?>">Read More</a></button>
+          <a href="<?php echo the_permalink() ?>">Read More</a>
           </div>
           </div>
       </li>
